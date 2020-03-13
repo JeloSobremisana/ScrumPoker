28a450be60de16e30bbccd32538c965d8d49c3c2
carlocortes <carlo.cortes@axa.com.ph>
2020-03-13 16:38:44 +0800

    card ui added

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  var socket = io.connect('http://10.18.112.42:3000'); // INVOKE SERVER SIDE SOCKET
+  var socket = io.connect('http://localhost:3000'); // INVOKE SERVER SIDE SOCKET
 
   $('.carousel').carousel(); //INVOKE CAROUSEL CSS AND FUNCTIONALITY
 
@@ -15,7 +15,20 @@ $(document).ready(function () {
   //SET PLAYER IN DASHBOARD
   socket.on('successReg', function(data) {
     $('.notif').append('<div class="notif-holder"><p class="center-align">' + data.name + ' has joined!</p></div>').fadeOut(2000, 'swing');
-    $('.player-container').append('<div class="playerdata" id="' + data.id + '"><p>' + data.name + '</p></div>');
+    $('.player-container').append('<div class="playerdata" style="text-align:center" id="' + data.id + '"><p>' + data.name + '</p>' +
+    '<div class="flip-card">' +
+      '<div class="flip-card-inner">' +
+        '<div class="flip-card-front">' +
+          '<img src="../images/cards.jpg" alt="Card">' +
+        '</div>' +
+        '<div class="flip-card-back">' +
+          '<div class="inner-border"></div>' +
+          '<div class="upper-left score-' + data.id + '"></div>' +
+          '<div class="lower-right score-' + data.id + '"></div>' +
+          '<div class="center-wrapper valign-center"><h1 id="score-' + data.id + '"></h1></div>' +
+        '</div>' +
+      '</div>' +
+    '</div></div>');
   }); 
 
   $('.set-button').click(function() {
@@ -29,6 +42,10 @@ $(document).ready(function () {
   });
 
   socket.on('returnScore', function(data) {
-    $('.playerdata#'+data.id).append('<h1>' + data.score+ '</h1>');
+
+    
+    $('#score-'+data.id).append(data.score);
+    $('.upper-left.score-'+data.id).append(data.score);
+    $('.lower-right.score-'+data.id).append(data.score);
   });
 });
